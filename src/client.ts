0f38a97890164458225a35d10e3f832d66be4727
Satisha <satisharps@gmail.com>
2026-08-16 19:29:25 +0200

    Patch prologue, add last_index support, reorganize

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
 import {Client, Item, ItemsManager} from "archipelago.js";
 import {addMessage} from "./doc";
-import {handle_item} from "./Patches/items";
+import {handle_item} from "./item_handler";
 
 // Create a new instance of the Client class.
 export const client = new Client();
@@ -9,16 +9,105 @@ export const items_manager = new ItemsManager(client);
 export function init_client() {
     // TODO Connect when save loaded
     // TODO Datapackage
-    // TODO last item index
+    // TODO last item index: save and load it on the save file
     // TODO connection page
-    client.login("localhost:38281", "Player1", "Alabaster Dawn")
-        .then(() => console.log("Connected to the Archipelago server!"))
+    client.login(client_data.url, client_data.slot_name, "Alabaster Dawn")
+        .then(() => addMessage(`Connected to Archipelago as Player1`))
         .catch(console.error);
 
-    items_manager.on("itemsReceived", (items) => {
+    items_manager.on("itemsReceived", (items, starting_index) => {
         let item: Item;
+        if (starting_index == 0) {
+            items = items.slice(client_data.last_item_index);  // TODO verify
+        }
+        else if (starting_index != client_data.last_item_index + 1) {
+            // TODO Ask for item sync
+        }
+
         for (item of items) {
-            handle_item(item.name);
+            handle_item(item);
+            client_data.last_item_index += 1;
         }
     });
 }
+
+class ClientData {
+    url: string;
+    slot_name: string;
+    password: string | null;
+
+    alias: string;
+
+    last_item_index: number;
+
+    constructor() {
+        this.url = "http://localhost:38281";  // TODO
+        this.slot_name = "Player1";
+        this.password = null;
+
+        this.alias = "Player1";
+
+        this.last_item_index = 0;
+    }
+
+    // TODO use JSON instead ?
+    // Export data as an Object, to store it in the save file
+    export_state(): Object {
+        return {
+            url: this.url,
+            slot_name: this.slot_name,
+            password: this.password,
+            alias: this.alias,
+            last_item_index: this.last_item_index,
+        }
+    }
+    
+    // Import the client state from the save file data
+    import_state(data: any) {
+        this.url = data.url;
+        this.slot_name = data.slot_name;
+        this.password = data.password;
+        this.alias = data.alias;
+        this.last_item_index = data.last_item_index;
+    }
+}
+
+export const client_data = new ClientData();
+
+// Class to track if a location check is due to receiving an AP item
+class ItemFlags {
+    elemID: number;
+    weaponKey: string;
+    constructor() {
+        this.elemID = 0;
+        this.weaponKey = "";
+    }
+    gaveElem(value: number) {
+        // Received an element (with value its ID) through an AP item
+        this.elemID = value;
+    }
+    checkedElem(value: number): boolean {
+        // Called in the ElementTracker hook when detecting a new element,
+        // return false if the change is caused by receiving an item.
+        if (value == this.elemID) {
+            this.elemID = 0;
+            return false;
+        }
+        return true;
+    }
+    gaveWeapon(value: string) {
+        // Received a weapon (with value its key) through an AP item
+        this.weaponKey = value;
+    }
+    checkedWeapon(value: string): boolean {
+        // Called in the WeaponTracker hook when detecting a new weapon,
+        // return false if the change is caused by receiving an item.
+        if (value == this.weaponKey) {
+            this.weaponKey = "";
+            return true;
+        }
+        return false;
+    }
+}
+
+export const item_flags = new ItemFlags()
---
+++ src/item_handler.ts
@@ -0,0 +1,104 @@
+import {terra} from "@project-selene/api";
+import {Item} from "archipelago.js";
+
+import {addMessage, addDebug} from "./doc";
+import {item_name_data} from "./item_name_gamedata";
+import {item_flags, client_data} from "./client";
+
+type LoadState = {
+    melee: string[];
+    ranged: string[];
+}
+
+export function handle_item(item: Item) {
+    let item_data = item_name_data.get(item.name);
+    if (!item_data) {
+        addMessage("Warning: Unknown item name received: " + item.name);
+        return;
+    }
+
+    if (item_data.name.startsWith("WEAPON:")) {
+        let weapon = item_data.name.substring("WEAPON:".length);
+        addDebug("Unlock " + weapon);
+        terra.g_player.combat.setWeaponUnlock(weapon, true);
+        item_flags.gaveWeapon(weapon);
+    }
+    else if (item_data.name.startsWith("ELEMENT:")) {
+        addDebug("Unlock " + item_data.name)
+        let elemID = Number(item_data.name.substring("ELEMENT:".length));
+        // TODO remove once init file is called
+        terra.g_player.setCore(18, true);  // Element change
+        terra.g_player.setCore(24, true);  // Loadouts
+        terra.g_player.setCore(elemID, true);
+        item_flags.gaveElem(elemID);
+
+        let melee: string[] = terra.g_player.combat.getMeleeWeaponList();
+        let range: string[] = terra.g_player.combat.getRangedWeaponList();
+        let elem: number = terra.g_player.combat.getTotalElementsUnlocked();
+
+        // TODO maybe do that on setup
+        // Equip weapons manually (with duplicates) if not enough weapons available to fill all slots
+        if (melee.length < 2 || range.length < 2) {  // TODO elem instead of 2
+            terra.g_player.combat.setLoadout(0);
+
+            let state: LoadState = {  // Force duplicates on the loadout
+                melee: [melee[0], melee[0], melee[0], melee[0]],
+                ranged: [range[0], range[0], range[0], range[0]]
+            };
+            for (let i = 0; i < terra.g_player.combat.loadouts.length; i++) {
+                terra.g_player.combat.loadouts[i].setState(state);
+            }
+            terra.g_player.combat.setLoadout(1);  // Reload loadout 0 so the weapons get equipped on the new element
+            terra.g_player.combat.setLoadout(0);
+        }
+        else {
+            terra.g_player.autoEquipWeapons();  // This is what the game usually does
+        }
+    }
+        // TODO Items for party members
+        //else if (item_data.name.startsWith("PARTY:")) {
+        //    let member = item_data.name.substring("PARTY:".length);
+        //    addDebug("Unlock " + member);
+        //    terra.g_party.addPartyMember("filia");
+    //}
+    else if (item_data.name == "Divine Connection") {
+        terra.g_player.combat.increaseSyncLevel();
+    }
+    else if (item_data.name.startsWith("PLOT:")) {
+        let plot_data = item_data.name.substring("PLOT:".length).split(".");
+        if (plot_data.length != 2) {
+            addMessage(`Failed to parse ${item_data.name}: expected it to split into two parts.`);
+        }
+        else {
+            addDebug(`Progress ${plot_data[0]} to ${plot_data[1]}`);
+            terra.g_plot.progressPlotToStateC(plot_data[0], plot_data[1]);
+        }
+    }
+    else if (item_data.name.startsWith("CL:")) {
+        let area = item_data.name.substring("CL:".length);
+        addDebug(`Progress  community level for ${area}`);
+        terra.g_plot.progressPlotToNextState("ap_" + area);
+    }
+    else if (item_data.name == "test") {
+        //terra.g_plot.progressPlotToStateC("ch1c", "goToRemisRock");
+        //let party_data = new PartyMember();
+        //let member_data = party_data.get("filia");
+        //terra.g_party.addPartyMember(member_data);
+
+        //terra.g_plot.progressPlotToStateC("ch2b", "traineeFlashback");
+        //terra.g_scene.teleport("start.north.north-03-dungeon", "");
+    }
+    else {
+        terra.g_player.inventory.addItem(item_data.name, item_data.qty);
+    }
+
+    display_item_message(item.name, item.sender.alias)
+}
+
+function display_item_message(item_name: string, sender: string) {
+    if (sender == client_data.alias) {
+        addMessage(`Found ${item_name}`);
+    } else {
+        addMessage(`Received ${item_name} from ${sender}`);
+    }
+}
